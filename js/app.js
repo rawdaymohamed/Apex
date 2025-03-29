aa6fdc126c7b7ec9bcc63e7577d887728c5030db
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-03-30 00:37:17 +0200

    Feat: Add slider design

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    $('#hero-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 1,
+        smartSpeed: 1000,
+        dots: false,
+        responsive: {
+            0: {
+
+            },
+            600: {
+
+            },
+            1000: {
+
+            }
+        }
+    })
+})
